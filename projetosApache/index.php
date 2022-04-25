8d3e04f840effbb2f8c1e483cd5ad8364c9576eb
portoedu <eduardo.porto@acad.pucrs.br>
2022-04-24 22:06:34 -0300

    colocando arquivos de testes com php, html e css

---
+++ projetosApache/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Meu primeiro site PHP!</title>
+
+
+        <body>
+
+            <?php   
+                for ($i = 0 ; $i <10 ; $i++)
+                {
+                    print( "Linha número " . $i . "<br />");
+                }
+            
+            ?>
+
+
+        </body>
+
+    </head>
+</html>
